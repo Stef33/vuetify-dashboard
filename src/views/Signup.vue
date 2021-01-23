e092376e4d7dc72da6d7bbf374e919f02932a99b
Stephane <slevrai@yahoo.fr>
2021-01-23 17:38:45 +0100

    Add toggle theme button

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,8 +3,8 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="email" type="email"></v-text-field>
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field label="email" type="email" v-model="email" :rules="emailRules" required></v-text-field>
           <v-autocomplete 
             label="Which browser you use ?"
             :items="browsers"></v-autocomplete>
@@ -34,8 +34,11 @@
                 min="1950-01-01"
                 @change="save"></v-date-picker>
             </v-menu>
-            <v-checkbox label="Agree to terms & conditions"></v-checkbox>
-            <v-btn type="submit" color="primary">Submit</v-btn>
+            <v-checkbox label="Agree to terms & conditions" v-model="agreeToTerms" :rules="agreeToTermsRules" required></v-checkbox>
+            <v-btn type="submit" color="primary" class="mr-4" :disabled="!formValidity">Submit</v-btn>
+            <v-btn color="success" @click="validateForm" class="mr-4">Validate Form</v-btn>
+            <v-btn color="warning" @click="resetValidation" class="mr-4">Reset Validation</v-btn>
+            <v-btn color="error" @click="resetForms" class="mr-4">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -45,6 +48,11 @@
 <script>
 export default {
   data: () => ({
+    agreeToTerms: false,
+    agreeToTermsRules: [
+      value =>
+        !!value || 'You must agree to the terms and conditions to sign up for an account.'
+    ],
     birthday: '',
     menu: false,
     browsers: [
@@ -53,7 +61,16 @@ export default {
       'Safari',
       'Edge',
       'Brave'
-    ]
+    ],
+    email: '',
+    emailRules: [
+      value => !!value || 'Email is required.',
+      value => value.indexOf('@') !== 0 || 'Email should have a username.',
+      value => value.indexOf('@') || 'Email should include an @ symbol.',
+      value => value.indexOf('.') - value.indexOf('@') > 1 || 'Email should contain a valid domain',
+      value => value.indexOf('.') <= value.length - 3 || 'Email should contain a valid domain extension.'
+    ],
+    formValidity: false,
   }),
   watch: {
     menu (val) {
@@ -61,9 +78,18 @@ export default {
     },
   },
   methods: {
+    resetForms() {
+      this.$refs.signUpForm.reset()
+    },
+    resetValidation () {
+      this.$refs.signUpForm.resetValidation()
+    },
     save (date) {
       this.$refs.menu.save(date)
     },
+    validateForm() {
+      this.$refs.signUpForm.validate()
+    }
   }
 }
 </script>
